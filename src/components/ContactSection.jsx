fd26de513f7ae84ff0de4e15f0998e7a65a302e2
moiz-xyz <a.moizniaxi123@gmail.com>
2025-08-02 14:52:50 +0500

    resolved production issue

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -122,7 +122,7 @@ export const ContactSection = () => {
                   name="name"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="Pedro Machado..."
+                  placeholder="Abdul Moiz"
                 />
               </div>
 
